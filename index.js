097a89da438b6abc51912a0f9975bec1bdf3127f
dotzipfile <davenickmalone@gmail.com>
2018-05-04 00:35:21 +0100

    setup basic server

---
+++ index.js
@@ -0,0 +1,20 @@
+// Dependencies
+const experss = require('express');
+const fs = require('fs');
+const request = require('request');
+const cheerio = require('cheerio');
+
+// Declare app
+const app = express();
+
+// Config variables
+const port = 3000;
+
+// Define /scrape route
+app.get('/scrape', function(req, res) {
+  
+});
+
+app.listen(port, () => {
+  console.log('App is listening on port: ' + port);
+});
